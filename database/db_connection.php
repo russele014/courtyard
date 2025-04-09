1af1e09beeff949d811083cf13e691dc44b0bc78
russele014 <russele.b7@gmail.com>
2025-04-10 02:38:15 +0800

    dbconn

--- database/db_connection.php
+++ database/db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "user_accounts";
+$dbname = "courtyard_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
