477e2ca2cb6e5e10f44102abb3a40abe70c6074f
russele014 <russele.b7@gmail.com>
2025-04-10 17:07:10 +0800

    User Dashboard

---
+++ UserDash.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOA Account History</title>
+    <link rel="stylesheet" href="UserDash.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>HOME OWNER'S ASSOCIATION OF MAIA ALTA, INC.</h1>
+            <h2>ACCOUNT HISTORY</h2>
+        </div>
+        
+        <div class="owner-info">
+            <div class="left-info">
+                <div class="owner-name">CAJAYON, FREDERICK D.</div>
+                <div class="property-info">Blk 025, Lot 0003</div>
+                <div class="property-info">Lot Area : 88</div>
+            </div>
+            <div class="right-info">
+                <div class="total-amount">Total Amount Due: <span class="php-currency">PhP</span> <span class="highlight-cell" id="total-due"></span></div>
+            </div>
+        </div>
+        
+        <table>
+            <thead>
+                <tr>
+                    <th rowspan="2">MONTH</th>
+                    <th rowspan="2">HOA DUES</th>
+                    <th colspan="12" class="penalty-header">PENALTY</th>
+                    <th rowspan="2">TOTAL PENALTY</th>
+                    <th rowspan="2">HOA DUES + PENALTY</th>
+                    <th rowspan="2">DATE</th>
+                    <th rowspan="2">PARTICULARS</th>
+                    <th rowspan="2">OR#</th>
+                    <th rowspan="2">AMOUNT PAID</th>
+                    <th rowspan="2">BAL</th>
+                </tr>
+                <tr>
+                    <th>JAN</th>
+                    <th>FEB</th>
+                    <th>MAR</th>
+                    <th>APRIL</th>
+                    <th>MAY</th>
+                    <th>JUNE</th>
+                    <th>JULY</th>
+                    <th>AUG</th>
+                    <th>SEPT</th>
+                    <th>OCT</th>
+                    <th>NOV</th>
+                    <th>DEC</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="month-column">Arr/Billed</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Jan-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Feb-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Mar-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Apr-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">May-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Jun-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Jul-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Aug-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Sep-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Oct-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Nov-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="month-column">Dec-25</td>
+                    <td></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td class="penalty-columns"></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        
+        <div class="total-amount">
+            TOTAL AMOUNT DUE: <span class="php-currency">PhP</span> <span class="highlight-cell" id="total-amount-due"></span>
+        </div>
+    </div>
+</body>
+</html>
