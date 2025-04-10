a9685ff564c53a4ec09c40a0089882694b86c55e
russele014 <russele.b7@gmail.com>
2025-04-10 22:59:25 +0800

    update

--- admin/dash_admin.php
+++ admin/dash_admin.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="stylesheet" href="../res/css/dash_admin.css">
+<link rel="stylesheet" href="res/css/dash_admin.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
