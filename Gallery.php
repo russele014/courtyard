f859763bf947ef9334936d9e3b4c02965648e0c1
russele014 <russele.b7@gmail.com>
2025-05-19 01:14:14 +0800

    new gallery

---
+++ Gallery.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Two Rows Two Columns Grid Alignment</title>
+  <link rel="stylesheet" href="navbar.css" />
+    <link rel="stylesheet" href="Gallery.css" />
+</head>
+
+<body>
+    <nav class="navbar">
+      <div class="navbar-left">The Courtyard of Maia Alta</div>
+      <ul class="navbar-right">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Gallery.php">Gallery </a></li>
+        <li><a href="News.php">News</a></li>
+        <li><a href="#about-container">About Us</a></li>
+        <li><a href="#officers-intro">History</a></li>
+        <li><a href="Login.php" class="logout-btn">Login</a></li>
+      </ul>
+    </nav>
+  <div class="container">
+    <div class="image1">
+      <img src="Endyr.jpg" alt="Image Left Top" />
+    </div>
+    <div class="text1">
+      <h1>End Year Party 2024 Heading Left Top</h1>
+      <p>Lorem ipsum dolor sit down amet, consectetur adipiscing elite, sed is eiusmod short-term incididunt ut performance and dolore magna aliqua</p>
+    </div>
+
+    <div class="text2">
+      <h1>Christmas Party</h1>
+      <p>Lorem ipsum dolor sit down amet, consectetur adipiscing elite, sed is eiusmod short-term incididunt ut performance and dolore magna aliqua</p>
+    </div>
+    <div class="image2">
+      <img src="Cmas.jpg" alt="Image Right Bottom" />
+    </div>
+  </div>
+</body>
+</html>
